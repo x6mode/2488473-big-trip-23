d127a1f426bc38a6631dfc40f526b15bd7439389
x6mode <x6modee@gmail.com>
2024-05-02 14:52:27 +0300

    исправление методов массива и обрезка моков

--- src/mock/mock-data.js
+++ src/mock/mock-data.js
@@ -1,4 +1,4 @@
-import getRandomArrayElement, { getRandomNumber } from '../utils.js';
+import getRandomArrayElement from '../utils.js';
 
 const mockRoutes = [
   {
@@ -54,7 +54,7 @@ const mockRoutesEdit = {
   dateTo: '2019-07-11T11:22:13.375Z',
   price: 259,
   offers: ['seats', 'comfort'],
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.'.split('. ').slice(getRandomNumber(0, 10), 11).join('. '),
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 };
 
 const getRandomRoute = () => getRandomArrayElement(mockRoutes);
--- src/presenter/first-present.js
+++ src/presenter/first-present.js
@@ -2,18 +2,15 @@ import { render } from '../render.js';
 import RouteView from '../view/point-route.js';
 import EditView from '../view/edit-point.js';
 
-
 export default class Presenter {
-  constructor({routes, routesEdit}) {
+  constructor({ routes, routesEdit }) {
     this.routes = routes;
     this.routesEdit = routesEdit;
   }
 
   init() {
-    for (let i = 0; i < this.routes.length; i++) {
-      render(new RouteView({route: this.routes[i]}), document.querySelector('.trip-events__list'));
-    }
+    this.routes.forEach((el) => render(new RouteView({ route: el }), document.querySelector('.trip-events__list')));
 
-    render(new EditView({routesEdit: this.routesEdit}), document.querySelector('.trip-events__list'));
+    render(new EditView({ routesEdit: this.routesEdit }), document.querySelector('.trip-events__list'));
   }
 }
